2e674b54bcaa76e44e66c997d25ecb1b3d4a7905
Rafli <standinsmel@gmail.com>
2025-11-10 18:40:18 +0700

    resolve not found problem

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // GitHub Pages project site: https://USERNAME.github.io/u-sehat-badminton/
+  // GitHub Pages project site: https://aeschyleaa.github.io/u-sehat-badminton/
   base: '/u-sehat-badminton/',
   plugins: [react()],
 })
